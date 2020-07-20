28ca3f19d4bc5a7163887f0cab2b39cabae9e8a4
ARKAR-PHYO <arkarphyo.edu@gmail.com>
2020-07-20 21:05:09 +0630

    Generate fake answers

--- app/Answer.php
+++ app/Answer.php
@@ -21,4 +21,13 @@ class Answer extends Model
         $markdown = new CommonMarkConverter(['allow_unsafe_links' => false]);
         return $markdown->convertToHtml($this->body);
     }
+
+    public  static function boot()
+    {
+       parent::boot();
+
+       static::created(function($answer){
+           $answer->question->increment('answers_count');
+       });
+    }
 }
