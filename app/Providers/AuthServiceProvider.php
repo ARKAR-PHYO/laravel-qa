cf6b656cff02ae7a14e903acbe1c108e430a6035
ARKAR-PHYO <arkarphyo.edu@gmail.com>
2020-07-21 15:43:12 +0630

    Install & Configur Laravel for Api Authentication

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 use Illuminate\Support\Facades\Gate;
 use App\Question;
 use App\Policies\QuestionPolicy;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -27,6 +28,6 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Passport::routes();
     }
 }
