64d2bea545b50ec24f3a4c12be4df3c149e4c7b4
ARKAR-PHYO <arkarphyo.edu@gmail.com>
2020-07-21 20:32:41 +0630

    Implement accept the answer as best answer

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -18,47 +18,62 @@
                         <a title="This answer is not useful" class="vote-down off">
                             <i class="fa fa-caret-down fa-3x"></i>
                         </a>
-                        <a title="Mark this answer as best answer" class="{{ $answer->status }} mt-2">
-                            <i class="fa fa-check fa-2x"></i>
-                        </a>
+                        @can('accept', $answer)
+                        <a title="Mark this answer as best answer" 
+                        class="{{ $answer->status }} mt-2"
+                        onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();"
+                        >
+                        <i class="fa fa-check fa-2x"></i>
+                    </a>
+                    <form action="{{ route('answers.accept', $answer->id) }}" id="accept-answer-{{ $answer->id }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                    @else
+                    @if ($answer->is_best)
+                    <a title="The question onwer accepted this answer as best answer" 
+                    class="{{ $answer->status }} mt-2">
+                    <i class="fa fa-check fa-2x"></i>
+                </a>
+                @endif
+                @endcan
+            </div>
+            <div class="media-body">
+                {!! $answer->body_html !!}
+                <div class="row">
+                    <div class="col-4">
+                        <div class="ml-auto">
+                            @can ('update', $answer)
+                            <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">{{ __('Edit') }}</a>
+                            @endcan
+                            
+                            @can ('delete', $answer)
+                            <form class="form-delete" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}" method="post">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are You Sure?')">{{ __('Del') }}</button>
+                            </form>
+                            @endcan
+                        </div>
                     </div>
-                    <div class="media-body">
-                        {!! $answer->body_html !!}
-                        <div class="row">
-                            <div class="col-4">
-                                <div class="ml-auto">
-                                    @can ('update', $answer)
-                                    <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">{{ __('Edit') }}</a>
-                                    @endcan
-                                    
-                                    @can ('delete', $answer)
-                                    <form class="form-delete" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}" method="post">
-                                        @method('DELETE')
-                                        @csrf
-                                        <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are You Sure?')">{{ __('Del') }}</button>
-                                    </form>
-                                    @endcan
-                                </div>
-                            </div>
-                            <div class="col-4"></div>
-                            <div class="col-4">
-                                <span class="text-muted">Answered {{ $answer->created_date }}</span>
-                                <div class="media mt-2">
-                                    <a href="{{ $answer->user->url }}" class="pr-2">
-                                        <img src="{{ $answer->user->avatar }}">
-                                    </a>
-                                    <div class="media-body mt-1">
-                                        <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
-                                    </div>
-                                </div>
+                    <div class="col-4"></div>
+                    <div class="col-4">
+                        <span class="text-muted">Answered {{ $answer->created_date }}</span>
+                        <div class="media mt-2">
+                            <a href="{{ $answer->user->url }}" class="pr-2">
+                                <img src="{{ $answer->user->avatar }}">
+                            </a>
+                            <div class="media-body mt-1">
+                                <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
                             </div>
                         </div>
-                        
                     </div>
                 </div>
-                <hr>
-                @endforeach
+                
             </div>
         </div>
+        <hr>
+        @endforeach
     </div>
+</div>
+</div>
 </div>
