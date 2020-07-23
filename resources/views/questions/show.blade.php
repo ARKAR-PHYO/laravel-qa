292678191d051db617419854bce52eec7f0c8029
ARKAR-PHYO <arkarphyo.edu@gmail.com>
2020-07-23 14:28:13 +0630

    Implement many to many polymorphic relationship and make the vote question controls working

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -18,52 +18,67 @@
                     
                     <div class="media">
                         <div class="d-flex flex-column vote-controls">
-                            <a title="This question is useful" class="vote-up">
-                                <i class="fa fa-caret-up fa-3x"></i>
-                            </a>
-                            <span class="votes-count">1230</span>
-                            <a title="This question is not useful" class="vote-down off">
-                                <i class="fa fa-caret-down fa-3x"></i>
-                            </a>
-                            <a title="Click to mark as favorite question (Click again to undi)" 
-                            class="favorite mt-2 {{ Auth::guest() ? 'off' : ($question->is_favorited ? 'favorited' : '') }}"
-                            onclick="event.preventDefault(); document.getElementById('favorite-question-{{ $question->id }}').submit();"
+                            <a title="This question is useful" class="vote-up {{ Auth::guest() ? 'off' : '' }}"
+                            onclick="event.preventDefault(); document.getElementById('up-vote-question-{{ $question->id }}').submit();"
                             >
-                                <i class="fa fa-star fa-2x"></i>
-                                <span class="favorites-count">{{ $question->favorites_count }}</span>
-                            </a>
-                            <form action="/questions/{{ $question->id }}/favorites" id="favorite-question-{{ $question->id }}" method="POST" style="display: none;">
-                                @csrf
-                                @if ($question->is_favorited)
-                                    @method('DELETE')
-                                @endif
-                            </form>
-                        </div>
-                        <div class="media-body">
-                            {!! $question->body_html !!}
-                            <div class="float-right">
-                                <span class="text-muted">Asked {{ $question->created_date }}</span>
-                                <div class="media mt-2">
-                                    <a href="{{ $question->user->url }}" class="pr-2">
-                                        <img src="{{ $question->user->avatar }}">
-                                    </a>
-                                    <div class="media-body mt-1">
-                                        <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                                    </div>
-                                </div>
-                            </div>
+                            <i class="fa fa-caret-up fa-3x"></i>
+                        </a>
+                        <form action="/questions/{{ $question->id }}/vote" id="up-vote-question-{{ $question->id }}" method="POST" style="display: none;">
+                            @csrf
+                            <input type="hidden" name="vote" value="1">
+                        </form>
+                        <span class="votes-count">{{ $question->votes_count }}</span>
+                        
+                        <a title="This question is not useful" 
+                        class="vote-down {{ Auth::guest() ? 'off' : '' }}"
+                        onclick="event.preventDefault(); document.getElementById('down-vote-question-{{ $question->id }}').submit();"
+                        >
+                        <i class="fa fa-caret-down fa-3x"></i>
+                    </a>
+                    <form action="/questions/{{ $question->id }}/vote" id="down-vote-question-{{ $question->id }}" method="POST" style="display: none;">
+                        @csrf
+                        <input type="hidden" name="vote" value="-1">
+                    </form>
+                    
+                    <a title="Click to mark as favorite question (Click again to undi)" 
+                    class="favorite mt-2 {{ Auth::guest() ? 'off' : ($question->is_favorited ? 'favorited' : '') }}"
+                    onclick="event.preventDefault(); document.getElementById('favorite-question-{{ $question->id }}').submit();"
+                    >
+                    <i class="fa fa-star fa-2x"></i>
+                    <span class="favorites-count">{{ $question->favorites_count }}</span>
+                </a>
+                <form action="/questions/{{ $question->id }}/favorites" id="favorite-question-{{ $question->id }}" method="POST" style="display: none;">
+                    @csrf
+                    @if ($question->is_favorited)
+                    @method('DELETE')
+                    @endif
+                </form>
+            </div>
+            <div class="media-body">
+                {!! $question->body_html !!}
+                <div class="float-right">
+                    <span class="text-muted">Asked {{ $question->created_date }}</span>
+                    <div class="media mt-2">
+                        <a href="{{ $question->user->url }}" class="pr-2">
+                            <img src="{{ $question->user->avatar }}">
+                        </a>
+                        <div class="media-body mt-1">
+                            <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+</div>
+</div>
 
-    @include('answers._index', [
-        'answers' => $question->answers,
-        'answersCount' => $question->answers_count,
-    ])
+@include('answers._index', [
+'answers' => $question->answers,
+'answersCount' => $question->answers_count,
+])
 
-    @include('answers._create')
+@include('answers._create')
 </div>
 @endsection
