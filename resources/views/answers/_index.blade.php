2ae518afb02ef12d79d29a9f193578872b446f3f
ARKAR-PHYO <arkarphyo.edu@gmail.com>
2020-07-23 18:49:44 +0630

    Implement DRY Primciple on the application views

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -6,88 +6,47 @@
                     <h2>{{ $answersCount . " " . Str::plural('Answer', $answersCount) }}</h2>
                 </div>
                 <hr>
-                @include('layouts._messages')
-                @foreach ($answers as $answer)
                 
+                @include('layouts._messages')
+                
+                @foreach ($answers as $answer)
                 <div class="media">
-                    <div class="d-flex flex-column vote-controls">
-                        <a title="This answer is useful" class="vote-up {{ Auth::guest() ? 'off' : '' }}"
-                        onclick="event.preventDefault(); document.getElementById('up-vote-answer-{{ $answer->id }}').submit();"
-                        >
-                        <i class="fa fa-caret-up fa-3x"></i>
-                    </a>
-                    <form action="/answers/{{ $answer->id }}/vote" id="up-vote-answer-{{ $answer->id }}" method="POST" style="display: none;">
-                        @csrf
-                        <input type="hidden" name="vote" value="1">
-                    </form>
-                    <span class="votes-count">{{ $answer->votes_count }}</span>
-                    
-                    <a title="This answer is not useful" 
-                    class="vote-down {{ Auth::guest() ? 'off' : '' }}"
-                    onclick="event.preventDefault(); document.getElementById('down-vote-answer-{{ $answer->id }}').submit();"
-                    >
-                    <i class="fa fa-caret-down fa-3x"></i>
-                </a>
-                <form action="/answers/{{ $answer->id }}/vote" id="down-vote-answer-{{ $answer->id }}" method="POST" style="display: none;">
-                    @csrf
-                    <input type="hidden" name="vote" value="-1">
-                </form>
-                @can('accept', $answer)
-                <a title="Mark this answer as best answer" 
-                class="{{ $answer->status }} mt-2"
-                onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();"
-                >
-                <i class="fa fa-check fa-2x"></i>
-            </a>
-            <form action="{{ route('answers.accept', $answer->id) }}" id="accept-answer-{{ $answer->id }}" method="POST" style="display: none;">
-                @csrf
-            </form>
-            @else
-            @if ($answer->is_best)
-            <a title="The question onwer accepted this answer as best answer" 
-            class="{{ $answer->status }} mt-2">
-            <i class="fa fa-check fa-2x"></i>
-        </a>
-        @endif
-        @endcan
-    </div>
-    <div class="media-body">
-        {!! $answer->body_html !!}
-        <div class="row">
-            <div class="col-4">
-                <div class="ml-auto">
-                    @can ('update', $answer)
-                    <a href="{{ route('questions.answers.edit', [$answer->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">{{ __('Edit') }}</a>
-                    @endcan
-                    
-                    @can ('delete', $answer)
-                    <form class="form-delete" action="{{ route('questions.answers.destroy', [$answer->id, $answer->id]) }}" method="post">
-                        @method('DELETE')
-                        @csrf
-                        <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are You Sure?')">{{ __('Del') }}</button>
-                    </form>
-                    @endcan
-                </div>
-            </div>
-            <div class="col-4"></div>
-            <div class="col-4">
-                <span class="text-muted">Answered {{ $answer->created_date }}</span>
-                <div class="media mt-2">
-                    <a href="{{ $answer->user->url }}" class="pr-2">
-                        <img src="{{ $answer->user->avatar }}">
-                    </a>
-                    <div class="media-body mt-1">
-                        <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
+                    @include('shared._vote', [
+                            'model' => $answer
+                        ])
+                    <div class="media-body">
+                        {!! $answer->body_html !!}
+                        <div class="row">
+                            <div class="col-4">
+                                <div class="ml-auto">
+                                    @can ('update', $answer)
+                                    <a href="{{ route('questions.answers.edit', [$answer->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">{{ __('Edit') }}</a>
+                                    @endcan
+                                    
+                                    @can ('delete', $answer)
+                                    <form class="form-delete" action="{{ route('questions.answers.destroy', [$answer->id, $answer->id]) }}" method="post">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit" class="btn btn-sm btn-outline-danger" 
+                                            onclick="return confirm('Are You Sure?')">{{ __('Del') }}</button>
+                                    </form>
+                                    @endcan
+                                </div>
+                            </div>
+                            <div class="col-4"></div>
+                            <div class="col-4">
+                                @include('shared._author', [
+                                'model' => $answer,
+                                'lable' => 'answered'
+                                ])
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
+                <hr>
+                @endforeach
             </div>
         </div>
-        
     </div>
-</div>
-<hr>
-@endforeach
-</div>
-</div>
-</div>
 </div>
